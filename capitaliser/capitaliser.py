68911296427b0c261e9ceb775c57e09ad5e68549
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-02-25 14:00:27 +0800

    code for capitaliser

---
+++ capitaliser/capitaliser.py
@@ -0,0 +1,15 @@
+import random
+
+def capitaliser(prompt):
+    prompt = list(prompt)
+    word = []
+
+    for chr in prompt:
+        rand = random.randint(0,1)
+        if rand == 0: chr = chr.lower()
+        if rand == 1: chr = chr.upper()
+        
+        word.append(chr)
+
+    word = "".join(word)
+    return word
