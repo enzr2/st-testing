f0c2cffe09b88d286ce5cdb3ab38219e9748bef4
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-02-25 20:56:35 +0800

    changed the format to make it more proper

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,68 @@
-import streamlit as st
 
+import streamlit as st
 from coursework.coursework import coursework
 from capitaliser.capitaliser import capitaliser
 
-#code display stuff
+# Importing code display stuff
 from code import capitaliser_text
 from code import coursework_text
 
-st.title("ying bing loves a math!!!!11!11!!")
-st.write("das so cool")
+def main():
+    # Title
+    st.title("ying bing loves a math!!!!11!11!!")
+    st.write("das so cool")
 
-option = st.checkbox("click 4 A1 math")
-if option == True:
-    st.write("user has been bleesed with a1")
+    # Checkbox for A1
+    option = st.checkbox("click 4 A1 math")
+    if option:
+        st.write("user has been bleesed with a1")
 
-# text capitaliser
-st.write("---")
+    # Text capitaliser
+    st.write("---")
+    text_capitaliser()
 
-st.subheader("funny text capitaliser")
-prompt_capitaliser = st.text_input("input here: ",value="hee hee heeeee haweeeeeewwww")
-word = capitaliser(prompt_capitaliser)
-st.write(word)
+    # Coursework
+    st.write("---")
+    coursework_section()
 
-# coursework
-st.write("---")
+    # Code display
+    st.write("---")
+    code_display()
 
-st.subheader("Coursework wooo")
-prompt_coursework = st.text_input("input here:",value= "im very happy :)")
+    # Link to GitHub
+    st.write("---")
+    github_link()
 
-# output format [emotion, message1, massage2, url]
-output = coursework(prompt_coursework)
+def text_capitaliser():
+    st.subheader("funny text capitaliser")
+    prompt = st.text_input("input here: ", value="hee hee heeeee haweeeeeewwww")
+    st.write(capitaliser(prompt))
 
-if output[0] == 0:
-    st.write(output[1])
+def coursework_section():
+    st.subheader("Coursework wooo")
+    prompt = st.text_input("input here:", value="im very happy :)")
+    output = coursework(prompt)
 
-else:
-    st.write(output[1])
-    st.link_button("Play video",output[3])
-    st.write(output[2])
+    # Output format [emotion, message1, message2, url]
+    if output[0] == 0:
+        st.write(output[1])
+    else:
+        st.write(output[1])
+        st.link_button("Play video", output[3])
+        st.write(output[2])
 
-# code display
-st.write("---")
-option = st.selectbox("code to display",
-             ("capitaliser","coursework"))
+def code_display():
+    option = st.selectbox("code to display", ("capitaliser", "coursework"))
 
-if option == "capitaliser": st.code(capitaliser_text.text)
-if option == "coursework": st.code(coursework_text.text)
+    if option == "capitaliser":
+        st.code(capitaliser_text.text)
+    elif option == "coursework":
+        st.code(coursework_text.text)
 
-# link to github
-st.write("---")
+def github_link():
+    st.subheader("The github")
+    st.link_button("Github :100:", "https://github.com/Jakeboiegg/st-testing.git")
 
-st.subheader("The github")
-st.link_button("Github :100:","https://github.com/Jakeboiegg/st-testing.git")
+if __name__ == "__main__":
+    main()
+
